0fc51df7737f1bfb6503fe711fd478677cd4c96c
Timoty Granziero <timoty.granziero@protonmail.com>
2025-12-08 23:55:03 +0100

    chore: add json and zig

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -1,6 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "java", "cpp", "python", "lua" },
+    ensure_installed = {
+      "java",
+      "cpp",
+      "python",
+      "lua",
+      "json",
+      "zig",
+    },
   },
 }
